3acddb53ec778cf1caead5bcccc5c4d37cafdb2d
ZiKay22 <108334047+ZiKay22@users.noreply.github.com>
2026-03-13 22:15:59 +0400

    update site

--- src/Moda/Moad.tsx
+++ src/Moda/Moad.tsx
@@ -60,6 +60,44 @@ export const arr_l=[{
     img:"https://i.postimg.cc/4xbW9Jxp/photo-2025-11-01-11-50-55.jpg"
 },
 
+{
+    id:15,
+    img:"https://i.postimg.cc/pXMKXk1h/Whats-App-Image-2026-03-08-at-09-47-00.jpg"
+},
+
+{
+    id:16,
+    img:"https://i.postimg.cc/qMrCdqzX/Whats-App-Image-2026-03-08-at-09-47-00-(1).jpg"
+},
+
+{
+    id:17,
+    img:"https://i.postimg.cc/wMRPqj8b/photo-2026-03-13-21-54-03.jpg"
+},
+{
+    id:18,
+    img:"https://i.postimg.cc/fTw2qCJ3/photo-2026-03-13-21-58-25.jpg"
+},
+{
+    id:19,
+    img:"https://i.postimg.cc/DyHG4yVH/photo-2026-03-13-22-00-52.jpg"
+},
+
+{
+    id:20,
+    img:"https://i.postimg.cc/g2K6SyL0/photo-2026-03-13-22-04-12.jpg"
+},
+
+{
+    id:20,
+    img:"https://i.postimg.cc/wTQvgqqD/photo-2026-03-13-22-04-16.jpg"
+},
+
+{
+    id:21,
+    img:"https://i.postimg.cc/P5Fd5NB6/photo-2026-03-13-22-04-19.jpg"
+},
+
 ]
 
 export const Moda=()=>{
